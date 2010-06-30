ec492263cac5aa8482091cb56e087937982e5df7
dnoseda <damian.noseda@mercadolibre.com>
2010-06-30 10:40:03 -0300

    este es todo el plugin, hay que probar

--- grails-app/conf/PreconditionsBootStrap.groovy
+++ grails-app/conf/PreconditionsBootStrap.groovy
@@ -1,31 +1,12 @@
-import org.codehaus.groovy.grails.orm.hibernate.support.ClosureEventTriggeringInterceptor as Events
-
+import com.google.common.base.Preconditions;
 class BootStrap {
  def grailsApplication
 
- def excludedProps = [Events.ONLOAD_EVENT,
-    Events.BEFORE_DELETE_EVENT, Events.AFTER_DELETE_EVENT,
-    Events.BEFORE_INSERT_EVENT, Events.AFTER_INSERT_EVENT,
-    Events.BEFORE_UPDATE_EVENT, Events.AFTER_UPDATE_EVENT]
-
   def init = { servletContext ->
-     grailsApplication.controllerClasses.each{ domainClass ->
-         domainClass.metaClass.part= { m ->
-             def map= [:]
-             if(m.'include'){
-                 m.'include'.each{
-                     map[it]= delegate."${it}"
-                 }
-             }else if(m.'except'){
-                 m.'except'.addAll excludedProps
-                 def props= domainClass.persistentProperties.findAll {
-                     !(it.name in m.'except')
-                 }
-                 props.each{
-                     map[it.name]= delegate."${it.name}"
-                 }
-             }
-             return map
+     grailsApplication.controllerClasses.each{ controllerClass ->
+     	controllerClass.metaClass.checkArgument= { conditionObject, message ->
+     		boolean condition = conditionObject ? true: false
+     		Preconditions.checkArgument(condition, message)     		
          }
      }
   }
