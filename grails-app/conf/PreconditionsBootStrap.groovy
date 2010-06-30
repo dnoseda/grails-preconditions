6e80f34792c723b75e176619ca07f48f14a4fe3c
dnoseda <damian.noseda@mercadolibre.com>
2010-06-30 11:24:41 -0300

    otras

--- grails-app/conf/PreconditionsBootStrap.groovy
+++ grails-app/conf/PreconditionsBootStrap.groovy
@@ -1,15 +1,18 @@
 import com.google.common.base.Preconditions;
 class PreconditionsBootStrap {
- def grailsApplication
-
-  def init = { servletContext ->
-     grailsApplication.controllerClasses.each{ controllerClass ->
-     	controllerClass.metaClass.checkArgument= { conditionObject, message ->
-     		boolean condition = conditionObject ? true: false
-     		Preconditions.checkArgument(condition, message)     		
-         }
-     }
-  }
-  def destroy = {
-  }
+	def grailsApplication
+	def genericCheckArgument = { conditionObject, message ->
+		boolean condition = conditionObject ? true: false
+		Preconditions.checkArgument(condition, message)     		
+	}
+	def init = { servletContext ->
+		grailsApplication.controllerClasses.each{ controllerClass ->
+			controllerClass.metaClass.checkArgument=genericCheckArgument 
+		}
+		grailsApplication.serviceClasses.each{ controllerClass ->
+			controllerClass.metaClass.checkArgument=genericCheckArgument 
+		}
+	}
+	def destroy = {
+	}
 }
